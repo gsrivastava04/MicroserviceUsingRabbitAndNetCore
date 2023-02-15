9111e97862dbcafba2ab4556416f981df24f761c
gsrivastava04 <gaurav06vit@gmail.com>
2023-02-15 12:29:54 +0530

    Basic  code changes for TransferLogs

--- MicroRabbit.Transfer.API/Program.cs
+++ MicroRabbit.Transfer.API/Program.cs
@@ -1,6 +1,16 @@
-var builder = WebApplication.CreateBuilder(args);
+using MediatR;
+using MicroRabbit.Domain.Core.Bus;
+using MicroRabbit.Infrastructure.Bus;
+using MicroRabbit.Transfer.Data.Context;
+using Microsoft.EntityFrameworkCore;
 
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddMediatR(typeof(Program));
 // Add services to the container.
+builder.Services.AddTransient<IEventBus, RabbitMQBus>();
+builder.Services.AddTransient<TransferDbContext>();
+var connectionString = builder.Configuration.GetConnectionString("TransferDbConnection");
+builder.Services.AddDbContext<TransferDbContext>(x => x.UseSqlServer(connectionString));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ TransferLog.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MicroRabbit.Transfer.Domain.Models
+{
+    public class TransferLog
+    {
+        public int Id { get; set; }
+        public string FromAccount { get; set; }
+        public string ToAccount { get; set; }
+        public decimal TransferAmount { get; set; }
+    }
+}
