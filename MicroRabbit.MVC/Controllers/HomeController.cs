e5edb5c4d2a0498aa03fcc3156137c834473188c
gsrivastava04 <gaurav06vit@gmail.com>
2023-02-15 20:19:13 +0530

    Added MVC project for demo

---
+++ MicroRabbit.MVC/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using MicroRabbit.MVC.Models;
+using MicroRabbit.MVC.Models.Dto;
+using MicroRabbit.MVC.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace MicroRabbit.MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ITransferService _transferService;
+
+        public HomeController(ITransferService transferService)
+        {
+            _transferService = transferService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Transfer(TransferViewModel viewModel)
+        {
+            TransferDto dto = new TransferDto
+            {
+                FromAccount = viewModel.FromAccount,
+                ToAccount = viewModel.ToAccount,
+                TransferAmount = viewModel.TransferAmount,
+            };
+            await _transferService.Transfer(dto);
+            return View();
+        }
+    }
+}
---
+++ MicroRabbit.MVC/Services/TransferService.cs
@@ -0,0 +1,24 @@
+﻿using MicroRabbit.MVC.Models.Dto;
+using Newtonsoft.Json;
+
+namespace MicroRabbit.MVC.Services
+{
+    public class TransferService : ITransferService
+    {
+        private readonly HttpClient _httpClient;
+
+        public TransferService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task Transfer(TransferDto transferDto)
+        {
+            var uri = "https://localhost:7206/api/Banking";
+            var content = new StringContent(JsonConvert.SerializeObject(transferDto),
+                System.Text.Encoding.UTF8, "application/json");
+            var response= await _httpClient.PostAsync(uri, content);
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
